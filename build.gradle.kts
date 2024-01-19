8ac946e3ffb0303da3c591cf7676b1002db82d6d
Karina Sofia Medina <kamedina@expediagroup.com>
2024-01-19 01:46:18 -0300

    docker-update
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("com.google.cloud.tools.jib") version "3.4.0"
 }
 
 group = "com.skm"
